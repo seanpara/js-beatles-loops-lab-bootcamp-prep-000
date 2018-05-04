776e19c49ac9dc6232bdfdf7c7957616faec39c9
Sean Logan Para <spara77@gmail.com>
2018-05-04 16:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ function theBeatlesPlay(musicians, instruments) {
   return beatlesPhrases
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
 
 function johnLennonFacts(facts) {
  var excitedFactsArray = [];
